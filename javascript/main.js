6eb75ac5ba33a0133e6e594de2f5f6fe1a231b8c
ziadmo5amed <161350029+ziadmo5amed@users.noreply.github.com>
2025-04-14 22:56:24 +0200

    slids

---
+++ javascript/main.js
@@ -0,0 +1,47 @@
+ 
+  const track = document.querySelector('.slider-track');
+  const nextBtn = document.getElementById('next');
+  const prevBtn = document.getElementById('prev');
+  const slideWidth = 320;  
+  let position = 0;
+
+  
+  function getMaxScroll() {
+    return track.scrollWidth - track.clientWidth;
+  }
+
+ 
+  nextBtn.addEventListener('click', () => {
+    const maxScroll = getMaxScroll();
+    if (Math.abs(position) < maxScroll) {
+      position -= slideWidth;
+    } else {
+      position = 0;  
+    }
+    updateSlider();
+  });
+
+  
+  prevBtn.addEventListener('click', () => {
+    if (position < 0) {
+      position += slideWidth;
+    }
+    updateSlider();
+  });
+
+  
+  function updateSlider() {
+    track.style.transform = `translateX(${position}px)`;
+  }
+
+ 
+  setInterval(() => {
+    const maxScroll = getMaxScroll();
+    if (Math.abs(position) < maxScroll) {
+      position -= slideWidth;
+    } else {
+      position = 0;
+    }
+    updateSlider();
+  },4000);
+ 
