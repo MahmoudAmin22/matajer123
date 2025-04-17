8ad33ba2d8255169c36f9338b29675955b172808
ziadmo5amed <161350029+ziadmo5amed@users.noreply.github.com>
2025-04-17 21:42:04 +0200

    finish home

--- javascript/main.js
+++ javascript/main.js
@@ -44,4 +44,44 @@
     }
     updateSlider();
   },4000);
- 
+
+
+
+  const toggleBtn = document.getElementById("toggle-theme");
+  const Payment = document.getElementById("Payment");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  
+
+  toggleBtn.textContent =  body.classList.contains("dark-mode") ? "☀️" : "🌙";
+   
+});
+
+let nav = document.querySelector("nav");
+let toggleMenu = document.querySelectorAll(".toggle-menu");
+window.addEventListener("scroll", scroll);
+
+function scroll() {
+  if (window.scrollY > 10) {
+    nav.style.top = "0";
+    nav.style.transition = "0.5s";
+    nav.style.boxShadow = "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset";
+    nav.style.background = "indigo";
+    
+    toggleMenu.forEach(link => {
+      link.style.color = "white";
+    });
+  }
+
+  else {
+    // nav.style.top = "-100px";
+    nav.style.transition = "0.5s";
+    nav.style.boxShadow = "none";
+    nav.style.background = "none";
+    toggleMenu.forEach(link => {
+      link.style.color = "gray";
+    });
+  }
+}
