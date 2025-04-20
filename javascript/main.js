15eac79ca772871795a3112b53941f501d019d1f
ziadmo5amed <161350029+ziadmo5amed@users.noreply.github.com>
2025-04-20 13:29:13 +0200

    pricing section

--- javascript/main.js
+++ javascript/main.js
@@ -85,3 +85,17 @@ function scroll() {
     });
   }
 }
+
+
+fetch("nav.html")
+.then(response => response.text())
+.then(data => {
+  document.getElementById("navbar").innerHTML = data;
+});
+
+
+fetch("footer.html")
+.then(response => response.text())
+.then(data => {
+  document.getElementById("footer").innerHTML = data;
+});
